75e1eb1c7df96d88e3e478f8d08a0ba191ba7feb
larryvsdata <ermanbekaroglu@gmail.com>
2018-10-03 23:05:46 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,14 +6,38 @@
 
 class EmailParser
   
-  attr_accessor : emails
+  attr_accessor :emails
   
   def initialize(emails)
     @emails=emails
   end
   
+  
   def parse
-    @emails.split(",").collect{ |email| email.strip}.uniq
+    
+    
+   if  @emails.include?(",") 
+    
+    @emails=@emails.split(",").map{ |email| email.strip}
+    
+    elsif  @emails.include?(" ") 
+    @emails=@emails.split(" ").map{ |email| email.strip}
   end
+  
+  
+   @emails=@emails.map{ |email|
+   if email.strip.include?(" ")
+   email.split(" ") 
+ else
+   email
+ end
+     
+   }.compact
+    
+  
+  
+  @emails.flatten.uniq
+  
+end
   
 end
